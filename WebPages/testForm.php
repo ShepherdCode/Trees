bb524010e0938282f9f8cfa68e5b7a5c41f191a3
e-foote <efoote01@rams.hepherd.edu>
2019-01-04 19:38:11 -0500

    Test php page asks user for TreeID

--- WebPages/testForm.php
+++ WebPages/testForm.php
@@ -2,33 +2,7 @@
 <body <h1>Enter TreeID</h1>
 </body>
 </html>
-<form action = "" method="post">
+<form action = "testResult.php" method="post">
 <input type = "text" name="search">
 <input type = "submit" name = "submit" value = "Search">
 </form>
-
-<?php
-$search_value=$_POST["search"];
-$servername = "127.0.0.1";
-$username = "root";
-$password = "shepherd";
-$dbname = "sheptrees";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn -> connect_error) {
-  die ("Connection failed: " . $conn -> connect_error);
-} else {
-
-$sql = "SELECT TreeID, ScientificName, CommonName FROM trees WHERE TreeID = $search_value;";
-$result = $conn -> query($sql);
-
-if ($result -> num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    echo "TreeID: " . $row["TreeID"] . "<br>" .
-        "Species: "  . $row["ScientificName"] . "<br>" .
-        "Common Name: " . $row["CommonName"] . "<br>"; }
-  } else {
-    echo "0 results";
-}
-}
-$conn -> close();
-?>
