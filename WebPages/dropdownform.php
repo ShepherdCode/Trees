8fdcef388a4c44493d9c69a91beee2ef26dc61aa
e-foote <efoote01@rams.hepherd.edu>
2019-01-14 16:38:36 -0500

    Drop down menu select from list of Genus

---
+++ WebPages/dropdownform.php
@@ -0,0 +1,29 @@
+<form action="dropdownresults.php" method="post">
+<select name="genuslist">
+  <?php
+  $servername = "127.0.0.1";
+  $username = "root";
+  $password = "shepherd";
+  $dbname = "sheptrees";
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if ($conn -> connect_error) {
+    die ("Connection failed: " . $conn -> connect_error);
+  } else {
+
+  $query = "SELECT genus_name FROM Genus;";
+  $result = $conn -> query($query);
+
+  if ($result -> num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+  ?>
+      <option value="<?php echo $row['genus_name']; ?>"> <?php echo $row['genus_name']; ?></option>;
+  <?php
+}} else {
+      echo "0 results";
+  }
+  }
+  $conn -> close();
+  ?>
+</select>
+  <input type="submit" name="select">
+</form>
