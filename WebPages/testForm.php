52ab2c6a1607479fac767ec51ffaacbe57998bce
e-foote <efoote01@rams.hepherd.edu>
2019-01-14 16:37:17 -0500

    Clean up code

--- WebPages/testForm.php
+++ WebPages/testForm.php
@@ -1,8 +1,5 @@
-<html>
-<body <h1>Enter TreeID</h1>
-</body>
-</html>
 <form action = "testResult.php" method="post">
+  Enter TreeID: <br>
 <input type = "text" name="search">
 <input type = "submit" name = "submit" value = "Search">
 </form>
