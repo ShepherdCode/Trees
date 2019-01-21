90419237a1946bb113c16f793a81f9f276aaef0c
e-foote <efoote01@rams.shepherd.edu>
2019-01-21 18:45:20 -0500

    modified code to conform to new normalized database

--- WebPages/testResult.php
+++ WebPages/testResult.php
@@ -9,17 +9,22 @@ if ($conn -> connect_error) {
   die ("Connection failed: " . $conn -> connect_error);
 } else {
 
-$query = "SELECT TreeID, ScientificName, CommonName FROM trees WHERE TreeID = $user_input;";
-$result = $conn -> query($query);
+  $query = "SELECT tree_id, scientific_name, common_name, ROUND(height,2) AS height, exposure_to_sunlight AS exposure
+            FROM tree_raw
+            WHERE tree_id = $user_input;";
+  $result = $conn -> query($query);
 
-if ($result -> num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    echo "TreeID: " . $row["TreeID"] . "<br>" .
-        "Species: "  . $row["ScientificName"] . "<br>" .
-        "Common Name: " . $row["CommonName"] . "<br>"; }
-  } else {
-    echo "0 results";
-}
-}
-$conn -> close();
+  if ($result -> num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      echo "<img class=\"pad\" width=25% src=\"../pictures/$user_input.jpg\"> <br>";
+      echo "Tree ID: " . $row["tree_id"] . "<br>" .
+          "Scientific Name: <i>"  . $row["scientific_name"] . "</i><br>" .
+          "Common Name: " . $row["common_name"] . "<br>" .
+          "Height: " . $row["height"] . " meters" . "<br>" .
+          "Exposure to Sunlight: " . $row["exposure"] . "<br>";}
+    } else {
+      echo "0 results";
+  }
+  }
+  $conn -> close();
 ?>
