f1215123b26a0e0150d3aa4f242eb635b4452788
e-foote <efoote01@rams.shepherd.edu>
2019-01-21 19:14:32 -0500

    Added error message for when the tree is not found

--- WebPages/results.php
+++ WebPages/results.php
@@ -57,17 +57,16 @@ if ($result -> num_rows > 0) {
         "Scientific Name: <i>"  . $row["scientific_name"] . "</i><br>" .
         "Common Name: " . $row["common_name"] . "<br>" .
         "Height: " . $row["height"] . " meters" . "<br>" .
-        "Exposure to Sunlight: " . $row["exposure"] . "<br>";}
+        "Exposure to Sunlight: " . $row["exposure"] . "<br>";
+    echo "</p><p class=\"space\" ></p><p class=\"pad\">";
+    echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et si turpitudinem fugimus in statu et motu corporis, quid est cur pulchritudinem non sequamur? Nam, ut paulo ante docui, augendae voluptatis finis est doloris omnis amotio. Haec para/doca illi, nos admirabilia dicamus. Duo Reges: constructio interrete. Longum est enim ad omnia respondere, quae a te dicta sunt. Sic enim censent, oportunitatis esse beate vivere. In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Sunt enim prima elementa naturae, quibus auctis vírtutis quasi germen efficitur. Longum est enim ad omnia respondere, quae a te dicta sunt. Parvi enim primo ortu sic iacent, tamquam omnino sine animo sint. Habes, inquam, Cato, formam eorum, de quibus loquor, philosophorum. Eorum enim est haec querela, qui sibi cari sunt seseque diligunt. Sed tamen est aliquid, quod nobis non liceat, liceat illis. Te enim iudicem aequum puto, modo quae dicat ille bene noris. Nunc omni virtuti vitium contrario nomine opponitur. Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; Omnes enim iucundum motum, quo sensus hilaretur. Sed haec quidem liberius ab eo dicuntur et saepius. Quid autem habent admirationis, cum prope accesseris? Dici enim nihil potest verius.";}
   } else {
-    echo "0 results";
+    echo "<font face=\"serif\" color=#036 size=25px> ERROR 404: TREE NOT FOUND </font>";
 }
 }
 $conn -> close();
 ?>
 </p>
-    <p class="pad" > Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et si turpitudinem fugimus in statu et motu corporis, quid est cur pulchritudinem non sequamur? Nam, ut paulo ante docui, augendae voluptatis finis est doloris omnis amotio. Haec para/doca illi, nos admirabilia dicamus. Duo Reges: constructio interrete. Longum est enim ad omnia respondere, quae a te dicta sunt. Sic enim censent, oportunitatis esse beate vivere. In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Sunt enim prima elementa naturae, quibus auctis vírtutis quasi germen efficitur.
-Longum est enim ad omnia respondere, quae a te dicta sunt. Parvi enim primo ortu sic iacent, tamquam omnino sine animo sint. Habes, inquam, Cato, formam eorum, de quibus loquor, philosophorum. Eorum enim est haec querela, qui sibi cari sunt seseque diligunt. Sed tamen est aliquid, quod nobis non liceat, liceat illis. Te enim iudicem aequum puto, modo quae dicat ille bene noris. Nunc omni virtuti vitium contrario nomine opponitur. Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; Omnes enim iucundum motum, quo sensus hilaretur. Sed haec quidem liberius ab eo dicuntur et saepius. Quid autem habent admirationis, cum prope accesseris? Dici enim nihil potest verius.
-    </p>
     <footer class="pad">
         Contact: Dr. Carol Z. Plautz, Professor of Biology
         [<a href="mailto:cplautz@shepherd.edu">email</a>].
